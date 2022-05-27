b857f44fc8ec1cf0c4ad5652dca0098705733e55
Anton Illarionov <anton.illarionov1991@gmail.com>
2022-05-27 16:59:26 +0300

    101%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -21,7 +21,7 @@ public class StatisticsServiceTest {
     void findMaxNew() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 32, 8, 6, 11, 11, 12};
         long expected = 32;
 
         long actual = service.findMax(incomesInBillions);
